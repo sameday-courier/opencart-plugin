763b8905e7432809ef33b656f396334d508116c4
Alexandru Soptareanu <alexandru.soptareanu@sameday.ro>
2022-02-02 16:41:49 +0200

    Bug fix.

--- system/library/sameday-classes/SamedayPersistenceDataHandler.php
+++ system/library/sameday-classes/SamedayPersistenceDataHandler.php
@@ -41,8 +41,8 @@ class SamedayPersistenceDataHandler implements SamedayPersistentDataInterface
      */
     public function set($key, $value): void
     {
-        $this->loader->model('extension/shipping/sameday');
-        $model = $this->registry->get('model_extension_shipping_sameday');
+        $this->loader->model('setting/setting');
+        $model = $this->registry->get('model_setting_setting');
         $key = $this->getKeyFormat($key);
         $data[$key] = $value;
         $model->addAdditionalSetting(self::OC_SETTING_SAMEDAY_CODE, $data);
