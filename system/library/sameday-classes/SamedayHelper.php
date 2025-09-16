cb1d63554847597e70dc4e29f750474240bb33e8
andrei <thesemeth@gmail.com>
2025-09-16 12:44:16 +0300

    added crossborder as ooh_service validation (showing map button for bg and hu)

--- system/library/sameday-classes/SamedayHelper.php
+++ system/library/sameday-classes/SamedayHelper.php
@@ -56,6 +56,7 @@ class SamedayHelper
     const OOH_SERVICES = [
         self::LOCKER_NEXT_DAY_SERVICE,
         self::SAMEDAY_PUDO_SERVICE,
+        self::LOCKER_NEXT_DAY_CROSSBORDER_SERVICE,
     ];
 
     const ELIGIBLE_TO_LOCKER = [
