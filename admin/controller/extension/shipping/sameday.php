92b19f3a27c1f6ad61dacd3fd7050e2ee97e7a23
andrei <thesemeth@gmail.com>
2026-04-09 11:05:43 +0300

    phpcs fix

--- admin/controller/extension/shipping/sameday.php
+++ admin/controller/extension/shipping/sameday.php
@@ -4,6 +4,7 @@ require_once DIR_SYSTEM . 'library/sameday-classes/SamedayShippingMethod.php';
 require_once DIR_SYSTEM . 'library/sameday-php-sdk/src/Sameday/autoload.php';
 require_once DIR_SYSTEM . 'library/samedayclasses.php';
 require_once DIR_SYSTEM . 'library/sameday-classes/SamedayTraitAdminController.php';
-class ControllerExtensionShippingSameday extends Controller{
+class ControllerExtensionShippingSameday extends Controller
+{
     use \SamedayTraitAdminController;
 }
